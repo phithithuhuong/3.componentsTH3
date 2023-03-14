ce7ca49babc6d298d7ca6d386dbe3aadd84acb85
phithithuhuong <thu872315@gmail.com>
2023-03-14 09:58:13 +0700

    th3 function/components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AppFunction from "./AppFunction";
+
+function App() {
+  return (
+    <div className="App">
+ <AppFunction firstNumber={10} lastNumber={20}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AppFunction.js
@@ -0,0 +1,6 @@
+function AppFunction(props){
+    return <div>
+        <h1>Total : {props.firstNumber +props.lastNumber }</h1>
+    </div>
+};
+export default AppFunction;
